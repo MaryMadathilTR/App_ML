b02c9a85176894658e3755b7757684dc3143e02e
Mary.MadathilTR <mary.madathil@thomsonreuters.com>
2024-10-14 08:48:24 +0530

    Push app to github

---
+++ app.py
@@ -0,0 +1,82 @@
+import dash
+from dash import html, dcc, Input, Output, State
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+
+# Initialize the Dash app
+app = dash.Dash(__name__)
+
+#Define the layout of the app
+app.layout = html.Div([
+    html.H1('Real Estate Price Prediction'),
+    html.Div([
+        html.Label('Distance to the nearest MRT station'),
+        dcc.Input(id='mrt', type='number', value=0),
+        html.Label('Number of convenience stores'),
+        dcc.Input(id='stores', type='number', value=0),
+        html.Label('Latitude'),
+        dcc.Input(id='lat', type='number', value=0),
+        html.Label('Longitude'),
+        dcc.Input(id='long', type='number', value=0),
+        html.Button('Predict', id='predict-button'),
+    ]),
+    html.Div(id='output')
+])
+
+
+# Define the callback
+@app.callback(
+    Output('output', 'children'),
+    Input('predict-button', 'n_clicks'),
+    State('mrt', 'value'),
+    State('stores', 'value'),
+    State('lat', 'value'),
+    State('long', 'value')
+)
+
+
+
+# Define the function that will be called when the button is clicked
+# def predict(n_clicks, mrt, stores, lat, long):
+#     if n_clicks is None:
+#         return ''
+    
+#     # Load the model
+#     model = LinearRegression()
+#     model.fit(X_train, y_train)
+    
+#     # Make a prediction
+#     prediction = model.predict([[mrt, stores, lat, long]])
+    
+#     return f'The predicted price is {prediction[0]:.2f}'
+def predict(n_clicks, mrt, stores, lat, long):
+    if n_clicks is None:
+        return ''
+    
+    # Load data
+    data = pd.read_csv('Real_Estate.csv')
+
+    # Prepare data
+    features = ['Distance to the nearest MRT station', 'Number of convenience stores','Latitude','Longitude']
+    target = 'House price of unit area'
+
+    X = data[features]
+    y = data[target]
+
+    # Split data
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+    
+    # Load the model
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+    
+    # Make a prediction
+    prediction = model.predict([[mrt, stores, lat, long]])
+    
+    return f'The predicted price is {prediction[0]:.2f}'
+
+# Run the app
+if __name__ == '__main__':
+    app.run_server(debug=True)
