27d022a9b1cffe35e8b3703a86b2d7e932eee1d4
Mary.MadathilTR <mary.madathil@thomsonreuters.com>
2024-10-14 09:26:47 +0530

    Updated the styles in the page

--- app.py
+++ app.py
@@ -13,13 +13,17 @@ app.layout = html.Div([
     html.H1('Real Estate Price Prediction'),
     html.Div([
         html.Label('Distance to the nearest MRT station'),
-        dcc.Input(id='mrt', type='number', value=0),
+        dcc.Input(id='mrt', type='number', value=0,
+                  style={'margin': '10px', 'padding': '10px'}),
         html.Label('Number of convenience stores'),
-        dcc.Input(id='stores', type='number', value=0),
+        dcc.Input(id='stores', type='number', value=0,
+                  style={'margin': '10px', 'padding': '10px'}),
         html.Label('Latitude'),
-        dcc.Input(id='lat', type='number', value=0),
+        dcc.Input(id='lat', type='number', value=0,
+                  style={'margin': '10px', 'padding': '10px'}),
         html.Label('Longitude'),
-        dcc.Input(id='long', type='number', value=0),
+        dcc.Input(id='long', type='number', value=0,
+                  style={'margin': '10px', 'padding': '10px'}),
         html.Button('Predict', id='predict-button'),
     ]),
     html.Div(id='output')
@@ -52,30 +56,33 @@ app.layout = html.Div([
     
 #     return f'The predicted price is {prediction[0]:.2f}'
 def predict(n_clicks, mrt, stores, lat, long):
-    if n_clicks is None:
+    if n_clicks > 0:
+        # Load data
+        data = pd.read_csv('Real_Estate.csv')
+
+        # Prepare data
+        features = ['Distance to the nearest MRT station', 'Number of convenience stores','Latitude','Longitude']
+        target = 'House price of unit area'
+
+        X = data[features]
+        y = data[target]
+
+        # Split data
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+        
+        # Load the model
+        model = LinearRegression()
+        model.fit(X_train, y_train)
+        
+        # Make a prediction
+        prediction = model.predict([[mrt, stores, lat, long]])
+        
+        return f'The predicted price is {prediction[0]:.2f}'
+    elif n_clicks > 0:
+        return 'Please enter the values'
+    else:
         return ''
-    
-    # Load data
-    data = pd.read_csv('Real_Estate.csv')
 
-    # Prepare data
-    features = ['Distance to the nearest MRT station', 'Number of convenience stores','Latitude','Longitude']
-    target = 'House price of unit area'
-
-    X = data[features]
-    y = data[target]
-
-    # Split data
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-    
-    # Load the model
-    model = LinearRegression()
-    model.fit(X_train, y_train)
-    
-    # Make a prediction
-    prediction = model.predict([[mrt, stores, lat, long]])
-    
-    return f'The predicted price is {prediction[0]:.2f}'
 
 # Run the app
 if __name__ == '__main__':
